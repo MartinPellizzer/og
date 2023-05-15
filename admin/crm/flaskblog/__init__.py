2aef5aac00a5303eb2814c08ad7f0fc9dcd74b0f
Martin Pellizzer <martinpellizzer@gmail.com>
2023-05-15 18:56:50 +0200

    '

--- admin/crm/flaskblog/__init__.py
+++ admin/crm/flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd88ecef6c5e0f68a3d37202aa2aee45f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
--- admin/crm/flaskblog/routes.py
+++ admin/crm/flaskblog/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from flaskblog import app
+from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 
@@ -22,6 +22,9 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
+    with app.app_context():
+        user = User.query.first()
+        print(user.password)
     return render_template('home.html', posts=posts)
 
 
@@ -34,8 +37,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'your account has been created. you are now able to log ink', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
     
 
