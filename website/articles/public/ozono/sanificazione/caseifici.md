375a22d32a1d94e2cd888449e4551579ebd70d71
Martin <martinpellizzer@gmail.com>
2023-09-18 12:31:04 +0200

    '

--- website/articles/public/ozono/sanificazione/caseifici.md
+++ website/articles/public/ozono/sanificazione/caseifici.md
@@ -9,7 +9,7 @@ last_update_date: 15/09/2023
 ![L'ozono migliora la salute della pelle](/assets/images//ozono-sanificazione-caseifici.jpg "L'ozono migliora la salute della pelle")
 
 
-## L'ozono quali applicazioni ha nell'industria lattiero-casearia?
+## Quali applicazioni ha l'ozono nell'industria lattiero-casearia?
 
 
 
@@ -57,16 +57,21 @@ Qui sotto alcuni utilizzi dell'ozono nel latte.
 - Riduce l'azoto ammoniacale totale dalle acque reflue del 80.2%, se usato a 5 L min-1 per 60 minuti, in pH 7.5 (Zhu et al., 2022). 
 
 
-
-
-
-
 ## L'ozono altera le proprietà sensoriali nei prodotti lattiero-caseari?
 
 Regarding the dairy industry, in most cases, no detectable changes from a sensorial point of view were detected after the ozone treatment (concentrations and exposure times)
 
 
-
-
-
-Studi dimostrano che l'ozono elimina il Cronobacter sakazaki dal latte in polvere, se utilizzato in concentrazioni di 2.8 mg L-1 per 120 minuti, riducendo la carica batterica di 2.71 log. Riduce anche gli psicrotrofi (batteri che crescono a temperature pari o inferiori a 7°C) dal latte scremato, se utilizzato in concentrazioni di 5-35 mg L-1 per 5-25 minuti. 
+- Migliora la qualità microbiologica del burro, se usato a 3.5 g h-1 per 60 minuti, a temperatura 8 °C (Sert et al., 2020). 
+- Riduce la qualità sensoriale del latte in polvere intero e scremato, se usato a 32 ppb (Kurtz et al., 1969). 
+- Aumenta la perossidazione dei lipidi del latte intero in polvere (Ipsen, 1989). 
+- Migliora il potere della schiumatura e la stabilità della schiuma, ma riduce la solubilità e la stabilità delle emulsioni del latte (siero), se usato in forma gassosa a 60 g h-1 per 15 minuti (Uzun et al., 2012). 
+- Migliora il potere della schiumatura e la stabilità della schiuma, ma riduce la solubilità e la stabilità delle emulsioni del latte (siero), se usato in forma acquosa a 4.5 ppm per 15 minuti (Uzun et al., 2012). 
+- Aumenta il potere della schiumatura e riduce la solubilità delle proteine del latte (siero), se usato a 20 mg L-1 per 30-480 minuti (Segat et al., 2014). 
+- Diminuisce la consistenza del prodotto, incrementa il diametro delle particelle, incrementa la viscosità del latte, incrementa la luminosità del latte scremato in polvere, se usato a 3.5 g h-1 per 120 minuti, a temperatura 24 °C (Sert and Mercan, 2021). 
+- Non causa alcuna differenza nelle proprietà sensoriali dello yogurt, se usato a 2.5-3 ppm per 60 secondi (Alexopoulos et al., 2017). 
+- Decrementa la qualità in termini di gusto, consistenza e colore del formaggio Feta, se usato per 60 minuti (Alexopoulos et al., 2017). 
+- Modifica il sapore nello strato appena sotto la crosta a causa della minor presenza di composti derivati dalla lipolisi (acidi grassi liberi e i loro derivati) del formaggio Ricotta Salata, Taleggio DOP e Gorgonzola DOP, se usato a 4 ppm per 8 minuti (Morandi et al., 2009). 
+- Non causa alcuna cambiamento nelle proprietà sensoriali del formaggio Pecorino, se usato in forma gassosa a 200 ppb per 8 ore per 150 giorni, a temperatura 12 °C con umidità ~85% (Grasso et al., 2022). 
+- Aumenta la spalmabilità e l'aroma, mentre diminuisce i carotenoidi e la stabilità all'ossidazione del burro, se usato a 3.5 g h-1 per 60 minuti, a temperatura 8 °C (Sert et al., 2020). 
+- Riduce consistenza, lucentezza, diametro delle particelle e spalmabilità del burro, se usato in forma acquosa a 0.3 mg L-1 per 30 minuti, a temperatura 2 °C (Sert and Mercan, 2020). 
--- website/main.py
+++ website/main.py
@@ -28,7 +28,7 @@ with open("database/dairy.csv", "r", encoding=encoding) as f:
 
 
 
-print(rows[0])
+# print(rows[0])
 
 
 # random.shuffle(rows)
@@ -61,6 +61,7 @@ for i, col in enumerate(rows[0]):
 
 for key, value in fields.items():
     print(key + " - " +  str(value))
+    pass
 
 rows = rows[1:]
 
@@ -88,7 +89,7 @@ def generate_line(i, row):
     tempo = row[fields['tempo']].strip()
     giorni = row[fields['giorni']].strip()
     
-    migliorie = row[fields['migliorie']].strip()
+    effetti_qualita = row[fields['effetti_qualita']].strip()
     
     temperatura = row[fields['temperatura']].strip()
     umidita = row[fields['umidita']].strip()
@@ -163,10 +164,107 @@ def generate_line(i, row):
 
     return f'- {sentence_formatted}\n'
 
+def generate_benefits(i, row):
+    studio = row[fields['studio']].strip()
+    anno = row[fields['anno']].strip()
+    
+    problema_articolo_determinativo = row[fields['problema_articolo_determinativo']].lower()
+    problema = row[fields['problema']].strip()
+    
+    prodotto_articolo_determinativo = row[fields['prodotto_articolo_determinativo']].lower()
+    prodotto_ad_benefici = row[fields['prodotto_ad_benefici']].lower()
+    prodotto = row[fields['prodotto']].strip()
+    sotto_prodotto = row[fields['sotto_prodotto']].strip()
+
+    riduzione_ad = row[fields['riduzione_articolo_determinativo']].lower()
+    riduzione = row[fields['riduzione']].strip()
+    
+    forma = row[fields['forma']].strip()
+    dose = row[fields['dose']].strip()
+    tempo = row[fields['tempo']].strip()
+    giorni = row[fields['giorni']].strip()
+    
+    effetti_qualita = row[fields['effetti_qualita']].strip()
+    
+    temperatura = row[fields['temperatura']].strip()
+    umidita = row[fields['umidita']].strip()
+    ph = row[fields['ph']].strip()
+
+    combinato = row[fields['combinato']].strip()
+
+    # if riduzione.strip() == '': 
+    #     return ''
+    
+    if forma.strip() != '': forma = f'in forma {forma} '
+    else: forma = ''
+    if dose.strip() != '': dose = f'a {dose} '
+    else: dose = ''
+    if tempo.strip() != '': tempo = f'per {tempo} '
+    else: tempo = ''
+    if giorni.strip() != '': giorni = f'per {giorni} '
+    else: giorni = ''
+
+    if riduzione_ad.strip() != '': riduzione_ad = f'{riduzione_ad} '
+    else: riduzione_ad = ''
+    if riduzione.strip() != '': riduzione = f'{riduzione} '
+    else: riduzione = ''
+
+    if temperatura.strip() != '': temperatura = f'a temperatura {temperatura}'
+    else: temperatura = ''
+    if umidita.strip() != '': umidita = f'con umidità {umidita}'
+    else: umidita = ''
+    if ph.strip() != '': ph = f'in pH {ph} '
+    else: ph = ''
+
+    if effetti_qualita.strip() != '': effetti_qualita = f'{effetti_qualita} '
+    else: effetti_qualita = ''
+    
+    if combinato.strip() != '': combinato = f'(combinato a {combinato}) '
+    else: combinato = ''
+
+    if (temperatura != '' or umidita != '' or ph != ''): conjunction_0 = ', '
+    else: conjunction_0 = ''
+
+    if (forma != '' or dose != '' or tempo != '' or giorni != ''): if_used = ', se usato'
+    else: if_used = ''
+
+    '''
+    Kurtz et al. (1969) latte scremato e intero in polvere 32 ppb 
+    '''
+
+    '''
+    Riduce la qualità sensoriale del latte in polvere intero e scremato 
+    (ma maggiormente di quello intero), 
+    se usato a 32 ppb (Kurtz et al., 1969).
+    '''
+
+    sentence = f'''
+        {effetti_qualita}
+        {prodotto_ad_benefici}{prodotto} {sotto_prodotto}
+        {if_used} 
+            {forma} 
+            {dose} 
+            {tempo} 
+            {giorni} 
+        {conjunction_0} 
+            {temperatura} 
+            {umidita} 
+            {ph} 
+
+        ({studio}, {anno}).
+        '''
+
+    print(sentence)
+
+    sentence_formatted = sentence.replace('\n', '')
+    # sentence_formatted = re.sub('|[^>]+!', '', sentence_formatted)
+    sentence_formatted = re.sub(' +', ' ', sentence_formatted)
+    sentence_formatted = sentence_formatted.replace(' ,', ',')
+    
+    print(sentence_formatted)
 
 
-
-
+    return f'- {sentence_formatted}\n'
 
 
 
@@ -177,55 +275,56 @@ with open('test.md', 'w', encoding=encoding) as f:
     f.write('')
 
 
+    
 
 text = ''
 for i, row in enumerate(rows):
-    # if len(text.split()) > 100: break
-
-    # LATTE
-    if row[fields['prodotto']].lower() == 'latte'.lower():
-        text += generate_line(i, row)
-        # text += '\n\n'
-
+    if row[fields['effetti_qualita']].lower() != ''.lower().strip():
+        text += generate_benefits(i, row)
 text = re.sub(' +', ' ', text)
-
 with open('test.md', 'a', encoding=encoding) as f:
     f.write('### Latte\n\n')
     f.write(f'{text}\n\n')
 
 
-
-text = ''
-for i, row in enumerate(rows):
-    # if len(text.split()) > 100: break
-
-    # ACQUE REFLUE
-    if row[fields['prodotto']].lower() == 'acque reflue'.lower():
-        text += generate_line(i, row)
-        # text += '\n\n'
-    
-text = re.sub(' +', ' ', text)
-
-with open('test.md', 'a', encoding=encoding) as f:
-    f.write('### Acque Reflue\n\n')
-    f.write(f'{text}\n\n')
+# quit()
 
 
 
-text = ''
-for i, row in enumerate(rows):
-    # if len(text.split()) > 100: break
 
-    # FORMAGGIO
-    if row[fields['prodotto']].lower() == 'formaggio'.lower():
-        text += generate_line(i, row)
-        # text += '\n\n'
-    
-text = re.sub(' +', ' ', text)
+# # LATTE
+# text = ''
+# for i, row in enumerate(rows):
+#     if row[fields['prodotto']].lower() == 'latte'.lower():
+#         text += generate_line(i, row)
+# text = re.sub(' +', ' ', text)
+# with open('test.md', 'a', encoding=encoding) as f:
+#     f.write('### Latte\n\n')
+#     f.write(f'{text}\n\n')
 
-with open('test.md', 'a', encoding=encoding) as f:
-    f.write('### Formaggio\n\n')
-    f.write(f'{text}\n\n')
+
+
+# # ACQUE REFLUE
+# text = ''
+# for i, row in enumerate(rows):
+#     if row[fields['prodotto']].lower() == 'acque reflue'.lower():
+#         text += generate_line(i, row)
+# text = re.sub(' +', ' ', text)
+# with open('test.md', 'a', encoding=encoding) as f:
+#     f.write('### Acque Reflue\n\n')
+#     f.write(f'{text}\n\n')
+
+
+
+# # FORMAGGIO
+# text = ''
+# for i, row in enumerate(rows):
+#     if row[fields['prodotto']].lower() == 'formaggio'.lower():
+#         text += generate_line(i, row)
+# text = re.sub(' +', ' ', text)
+# with open('test.md', 'a', encoding=encoding) as f:
+#     f.write('### Formaggio\n\n')
+#     f.write(f'{text}\n\n')
 
 
 with open('test.md', encoding=encoding) as f:
