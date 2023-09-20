974d8a475203dfe0d0650738c54029a6afacdd07
Martin Pellizzer <martinpellizzer@gmail.com>
2023-09-20 18:57:22 +0200

    '

--- website/generate_article.py
+++ website/generate_article.py
@@ -2,8 +2,27 @@ import re
 import csv
 import markdown
 
+from PIL import Image, ImageFont, ImageDraw, ImageColor 
+
+
 
 encoding = 'utf-8'
+
+def csv_get_rows(filename):
+    _rows = []
+    with open(f"database/{filename}.csv", "r", encoding=encoding) as f:
+        reader = csv.reader(f, delimiter="\\")
+        for i, line in enumerate(reader):
+            _rows.append(line)
+    return _rows
+
+    
+def row_to_dict(row):
+    fields = {}
+    for i, col in enumerate(row):
+        fields[col] = i
+    return fields
+
 
 table = 'meat'
 
@@ -13,6 +32,7 @@ with open(f"database/{table}.csv", "r", encoding=encoding) as f:
     reader = csv.reader(f, delimiter="\\")
     for i, line in enumerate(reader):
         rows.append(line)
+
 
 
 
@@ -123,6 +143,7 @@ def generate_line(i, row):
 
     return f'- {sentence_formatted}\n'
 
+
 def generate_benefits(i, row):
     studio = row[fields['studio']].strip()
     anno = row[fields['anno']].strip()
@@ -226,7 +247,6 @@ def generate_benefits(i, row):
     return f'- {sentence_formatted}\n'
 
 
-
 def get_line_data(row):
     data = {}
     for i, col in enumerate(row):
@@ -307,8 +327,10 @@ with open('test.md', 'w', encoding=encoding) as f:
 
 
 def generate_section(product):
-    text = ''
+    text_intro = ''
 
+    # GET LIST OF PROBLEMS AND STUDIES FORM PRODUCT
+    # TODO: remove duplicates
     problem_list = []
     study_list = []
     for i, row in enumerate(rows):
@@ -316,41 +338,56 @@ def generate_section(product):
             problem_list.append(row[fields['problem']].strip().lower())
             study_list.append(row[fields['study']].strip().lower())
 
-    print(problem_list)
-    print(study_list)
 
+    # GET PRODUCT AD 3
+    _rows = csv_get_rows('products')
+    _fields = row_to_dict(_rows[0])
 
-
-    text += f'''
-        L'ozono viene usato per trattare {product}
+    _product_ad = ''
+    _product = ''
+    for _row in _rows:
+        _product = _row[_fields['product']].strip().lower()
+        if _product == product:
+            _product_ad = _row[_fields['product_ad_3']].lower()
+            break
+  
+    # WRITE TEXT
+    text_intro += f'''
+        L'ozono viene usato per trattare {_product_ad}{_product}
         , eliminando problemi come {problem_list[0]} {problem_list[1]} e {problem_list[2]}
         , come dimostrato da diversi studi (ad esempio {study_list[0]} e {study_list[1]}).
     '''
-    text = text.replace('\n', '').strip()
-    text = re.sub(' +', ' ', text)
-    text += '\n\n'
+    text_intro = text_intro.replace('\n', '').strip()
+    text_intro = re.sub(' +', ' ', text_intro)
+    text_intro = text_intro.replace(' ,', ',')
+    text_intro += '\n\n'
 
      
     
     # INTRODUZIONE LISTA -----------------------------------------------
+    text_list_intro = ''
+
     for i, row in enumerate(rows):
         if row[fields['product']].strip().lower() == product.lower().strip():
             data = get_line_data(row)
             break
     
-    text += f'''
+    text_list_intro += f'''
         Ecco una lista di alcune applicazioni dell\'ozono 
         {data['product_ad_2']}{data['product']}:
     '''
-    text = text.replace('\n', '').strip()
-    text = re.sub(' +', ' ', text)
-    text += '\n\n'
+    text_list_intro = text_list_intro.replace('\n', '').strip()
+    text_list_intro = re.sub(' +', ' ', text_list_intro)
+    text_list_intro += '\n\n'
 
     # LISTA ------------------------------------------------------------
+    text_list = ''
     for i, row in enumerate(rows):
         if row[fields['product']].strip().lower() == product.lower().strip():
-            text += generate_line_application(row)
+            text_list += generate_line_application(row)
 
+    
+    text = text_intro + text_list_intro + text_list
     with open('test.md', 'a', encoding=encoding) as f:
         f.write(f'### {product.capitalize()}\n\n')
         f.write(f'{text}\n\n')
@@ -365,6 +402,43 @@ generate_section('ritagli di manzo')
 # generate_section('prosciutto')
 
 
+def text_split(text, font, img_w, px):
+    lines = text.strip().split('\n')
+
+    words = []
+    for line in lines:
+        if not line:
+            words.append('\n')
+        else:
+            line_words = line.split()
+            for word in line_words:
+                words.append(word)
+
+    lines = []
+    line = ''
+    for word in words:
+        if word == '\n':
+            lines.append(line)
+            lines.append('')
+            line = ''
+        else:
+            word_w = font.getsize(word)[0]
+            line_w = font.getsize(line)[0]
+            if word_w + line_w < img_w - px:
+                line += word + ' '
+            else:
+                lines.append(line)
+                line = word + ' '
+    lines.append(line)
+
+    return lines
+
+def lines_max_height(lines, font):
+    line_height = 0
+    for line in lines:
+        if line_height < font.getsize(line)[1]: 
+            line_height = font.getsize(line)[1]
+    return line_height
 
 
 def generate_image_sanitation(image_out_path):    
@@ -373,42 +447,59 @@ def generate_image_sanitation(image_out_path):
 
     draw = ImageDraw.Draw(img)
 
+    draw.rectangle(((0, 0), (img_w, 112)), fill=ImageColor.getrgb("#2563eb"))
     
     icon_w = 96
     icon_h = 96
 
-    icon = Image.open("assets/icons/milk.png").convert("RGBA")
-    icon = icon.resize((icon_w, icon_h))
-    img.paste(icon, (0, 0), icon)
+    icon_size = (96, 96)
+    icon_bg = Image.new('RGBA', icon_size, (0, 0, 0, 0))
+    icon_fg = Image.new("RGBA", icon_size, ImageColor.getrgb("#2563eb"))
+    icon_mask = Image.open('assets/icons/articles/milk.png').convert('L').resize(icon_size)
+    icon = Image.composite(icon_bg, icon_fg, icon_mask)
+    img.paste(icon, (0, 200), icon)
 
-    icon = Image.open("assets/icons/milk.png").convert("RGBA")
-    icon = icon.resize((icon_w, icon_h))
-    img.paste(icon, (200, 0), icon)
+    icon_size = (96, 96)
+    icon_bg = Image.new('RGBA', icon_size, (0, 0, 0, 0))
+    icon_fg = Image.new("RGBA", icon_size, ImageColor.getrgb("#2563eb"))
+    icon_mask = Image.open('assets/icons/articles/cheese.png').convert('L').resize(icon_size)
+    icon = Image.composite(icon_bg, icon_fg, icon_mask)
+    img.paste(icon, (200, 200), icon)
 
-    icon = Image.open("assets/icons/milk.png").convert("RGBA")
-    icon = icon.resize((icon_w, icon_h))
-    img.paste(icon, (400, 0), icon)
+    icon_size = (96, 96)
+    icon_bg = Image.new('RGBA', icon_size, (0, 0, 0, 0))
+    icon_fg = Image.new("RGBA", icon_size, ImageColor.getrgb("#2563eb"))
+    icon_mask = Image.open('assets/icons/articles/yogurt.png').convert('L').resize(icon_size)
+    icon = Image.composite(icon_bg, icon_fg, icon_mask)
+    img.paste(icon, (400, 200), icon)
 
-    icon = Image.open("assets/icons/milk.png").convert("RGBA")
-    icon = icon.resize((icon_w, icon_h))
-    img.paste(icon, (600, 0), icon)
-    
-    icon = Image.open("assets/icons/milk.png").convert("RGBA")
-    icon = icon.resize((icon_w, icon_h))
-    img.paste(icon, (100, 200), icon)
+    text_size = 40
+    text = 'Sanificazione ad ozono nell\'industria lattiero-casearia'
+    px = img_w - img_w * 0.9
+    pt = 8
 
-    icon = Image.open("assets/icons/milk.png").convert("RGBA")
-    icon = icon.resize((icon_w, icon_h))
-    img.paste(icon, (300, 200), icon)
+    font = ImageFont.truetype("fonts/arial.ttf", text_size)
 
-    icon = Image.open("assets/icons/milk.png").convert("RGBA")
-    icon = icon.resize((icon_w, icon_h))
-    img.paste(icon, (500, 200), icon)
+    lines = text_split(text, font, img_w, px) 
+    line_height = lines_max_height(lines, font) * 1.2
+    text_height = line_height * len(lines)
+
+
+    for i, line in enumerate(lines):
+        line_w = font.getsize(line)[0]
+        draw.text(
+            (img_w//2 - line_w//2, (line_height * i) + pt), 
+            line, 
+            (255,255,255), 
+            font=font
+            )
 
     img.convert('RGB').save(f'{image_out_path}')
-    # img.show()
+    img.show()
 
-# generate_image_sanitation('assets/images/articles/ozono-sanificazione-caseifici.jpg')
+generate_image_sanitation('assets/images/articles/ozono-sanificazione-industria-lattiero-casearia.jpg')
+
+quit()
 
 
 
---
+++ website/test.md
@@ -0,0 +1,57 @@
+### Carcasse di manzo
+
+L'ozono viene usato per trattare le carcasse di manzo, eliminando problemi come e. coli 0157:h7 salmonella typhimurium e e. coli 0157:h7, come dimostrato da diversi studi (ad esempio castillo et al. e castillo et al.).
+
+Ecco una lista di alcune applicazioni dell'ozono nelle carcasse di manzo:
+
+-  Riduce il livello di E. coli 0157:H7 dalle carcasse di manzo in modo significativo a bassa pressione, se usato in forma acquosa spruzzo a una pressione di a una pressione di 80 lb/in2 a una concentrazione di 95 mg/L (Castillo et al., 2003). 
+-  Riduce il livello di Salmonella typhimurium dalle carcasse di manzo in modo significativo a bassa pressione, se usato in forma acquosa spruzzo a una pressione di a una pressione di 80 lb/in2 a una concentrazione di 95 mg/L (Castillo et al., 2003). 
+-  Riduce il livello di E. coli 0157:H7 dalle carcasse di manzo in modo significativo a bassa pressione, se usato spruzzo a una pressione di a una pressione di 400 lb/in2 (Castillo et al., 2003). 
+-  Riduce il livello di Salmonella typhimurium dalle carcasse di manzo in modo significativo a bassa pressione, se usato spruzzo a una pressione di a una pressione di 400 lb/in2 (Castillo et al., 2003). 
+-  Riduce il livello di materiale fecale con vari patogeni dalle carcasse di manzo di 1.30 CFU/cm2, se usato in forma acquosa risciacquo a una concentrazione di 0.3-2.3 ppm (Reagan et al., 1996). 
+-  Riduce il livello di materiale fecale con vari patogeni dalle carcasse di manzo di 1.14 CFU/cm2, se usato risciacquo a una concentrazione di 5% (Reagan et al., 1996). 
+-  Riduce il livello di psicrotrofi dalle carcasse di manzo, se usato refrigeratore (Greer and Jones, 1989). 
+-  Riduce il livello di psicrotrofi dalle carcasse di manzo, se usato refrigeratore (Greer and Jones, 1989). 
+
+
+### Petto di manzo
+
+L'ozono viene usato per trattare il petto di manzo, eliminando problemi come e. coli e. coli e e. coli, come dimostrato da diversi studi (ad esempio gorman et al. e gorman et al.).
+
+Ecco una lista di alcune applicazioni dell'ozono nel petto di manzo:
+
+-  Riduce il livello di E. coli dal petto di manzo di 2.60-2.86 CFU/cm2, se usato in forma acquosa spruzzo a una concentrazione di 0.5% (Gorman et al., 1995). 
+-  Riduce il livello di E. coli dal petto di manzo di 2.60-2.86 CFU/cm2, se usato spruzzo a una concentrazione di 5% (Gorman et al., 1995). 
+-  Riduce il livello di E. coli dal petto di manzo in modo inferiore a ozono, se usato spruzzo a una concentrazione di 12% (Gorman et al., 1995). 
+-  Riduce il livello di E. coli dal petto di manzo in modo inferiore a ozono, se usato spruzzo a una concentrazione di 2% (Gorman et al., 1995). 
+-  Riduce il livello di E. coli dal petto di manzo in modo inferiore a ozono, se usato spruzzo a una concentrazione di 0.3% (Gorman et al., 1995). 
+
+
+### Ritagli di manzo
+
+L'ozono viene usato per trattare i ritagli di manzo, eliminando problemi come e. coli s. typhimurium e coliformi, come dimostrato da diversi studi (ad esempio stivarius et al. e stivarius et al.).
+
+Ecco una lista di alcune applicazioni dell'ozono nei ritagli di manzo:
+
+-  Riduce il livello di E. coli dai ritagli di manzo, se usato in forma acquosa a una concentrazione di 1% per un tempo di 7 minuti (Stivarius et al., 2002a). 
+-  Riduce il livello di S. typhimurium dai ritagli di manzo in modo significativo, se usato in forma acquosa a una concentrazione di 1% per un tempo di 7 minuti (Stivarius et al., 2002a). 
+-  Riduce il livello di coliformi dai ritagli di manzo, se usato in forma acquosa a una concentrazione di 1% per un tempo di 7 minuti (Stivarius et al., 2002a). 
+-  Riduce il livello di conta delle piastre aerobiche dai ritagli di manzo in modo significativo, se usato in forma acquosa a una concentrazione di 1% per un tempo di 7 minuti (Stivarius et al., 2002a). 
+-  Riduce il livello di E. coli dai ritagli di manzo in modo totale, se usato in forma acquosa a una concentrazione di 1% per un tempo di 15 minuti (Stivarius et al., 2002a). 
+-  Riduce il livello di S. typhimurium dai ritagli di manzo in modo totale, se usato in forma acquosa a una concentrazione di 1% per un tempo di 15 minuti (Stivarius et al., 2002a). 
+-  Riduce il livello di coliformi dai ritagli di manzo in modo totale, se usato in forma acquosa a una concentrazione di 1% per un tempo di 15 minuti (Stivarius et al., 2002a). 
+-  Riduce il livello di conta delle piastre aerobiche dai ritagli di manzo in modo totale, se usato in forma acquosa a una concentrazione di 1% per un tempo di 15 minuti (Stivarius et al., 2002a). 
+-  Riduce il livello di E. coli dai ritagli di manzo in modo efficace, se usato in forma acquosa a una concentrazione di 1% (Pohlman et al, 2002). 
+-  Riduce il livello di S. typhimurium dai ritagli di manzo in modo efficace, se usato in forma acquosa a una concentrazione di 1% (Pohlman et al, 2002). 
+-  Riduce il livello di coliformi dai ritagli di manzo in modo efficace, se usato in forma acquosa a una concentrazione di 1% (Pohlman et al, 2002). 
+-  Riduce il livello di conta delle piastre aerobiche dai ritagli di manzo in modo efficace, se usato in forma acquosa a una concentrazione di 1% (Pohlman et al, 2002). 
+-  Riduce il livello di E. coli dai ritagli di manzo in modo efficace, se usato in forma acquosa a una concentrazione di 1% (Pohlman et al, 2002). 
+-  Riduce il livello di S. typhimurium dai ritagli di manzo in modo efficace, se usato in forma acquosa a una concentrazione di 1% (Pohlman et al, 2002). 
+-  Riduce il livello di coliformi dai ritagli di manzo in modo efficace, se usato in forma acquosa a una concentrazione di 1% (Pohlman et al, 2002). 
+-  Riduce il livello di conta delle piastre aerobiche dai ritagli di manzo in modo efficace, se usato in forma acquosa a una concentrazione di 1% (Pohlman et al, 2002). 
+-  Riduce il livello di E. coli dai ritagli di manzo in modo efficace, se usato a una concentrazione di 200 ppm (Pohlman et al, 2002). 
+-  Riduce il livello di S. typhimurium dai ritagli di manzo in modo efficace, se usato a una concentrazione di 200 ppm (Pohlman et al, 2002). 
+-  Riduce il livello di coliformi dai ritagli di manzo in modo efficace, se usato a una concentrazione di 200 ppm (Pohlman et al, 2002). 
+-  Riduce il livello di conta delle piastre aerobiche dai ritagli di manzo in modo efficace, se usato a una concentrazione di 200 ppm (Pohlman et al, 2002). 
+
+
