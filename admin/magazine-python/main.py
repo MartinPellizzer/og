7dffe7dfc60599e8eb9a020f67092e47632874b7
Martin Pellizzer <martinpellizzer@gmail.com>
2024-02-07 18:52:45 +0100

    '

--- admin/magazine-python/main.py
+++ admin/magazine-python/main.py
@@ -41,60 +41,46 @@ def img_resize(image_path_in, image_path_out, w, h, quality=100):
 
     return output_path
 
-
-
-# for image_filename in os.listdir('in'):
-#     print(image_filename)
-#     image_path_in = 'in/' + image_filename
-#     image_path_out = 'out/' + image_filename.replace('.png', '.jpg')
-    
-#     img_resize(image_path_in, image_path_out, w, h 70)
-
-
-
-
-
-
-
-
+#####################################################################
+# ;MAIN
+#####################################################################
 a4_w, a4_h = 2480, 3508
-
-a4_mx = a4_w//100*5
-
-column_num = 3
-column_w = (a4_w - a4_mx*2)//column_num
-column_gap = a4_mx*0.5
-line_num = column_num-1
 
 img = Image.new("RGB", (a4_w, a4_h), "white")
 draw = ImageDraw.Draw(img)
 
-# DRAW MARGINS
-# draw.line((a4_mx, 0, a4_mx, a4_h), fill=128)
-# draw.line((a4_w - a4_mx, 0, a4_w - a4_mx, a4_h), fill=128)
 
-# DRAW COLUMNS
-# for i in range(line_num):
-#     draw.line((column_w*(i+1) + a4_mx - column_gap, 0, column_w*(i+1) + a4_mx  - column_gap, a4_h), fill='#a21caf')
-#     draw.line((column_w*(i+1) + a4_mx, 0, column_w*(i+1) + a4_mx, a4_h), fill=128)
-#     draw.line((column_w*(i+1) + a4_mx + column_gap, 0, column_w*(i+1) + a4_mx  + column_gap, a4_h), fill='#a21caf')
+
+a4_mx = a4_w//100*5
+a4_my = a4_h//100*5
+
+
+column_num = 3
+column_w = (a4_w - a4_mx*2)//column_num
+column_gap = a4_mx*0.5
+
+row_num = 32
+row_h = (a4_h - a4_my*2)//row_num
+row_gap = a4_my*0.5
+
+
+
+def get_coord(cs, rs, ce, re):
+    x = a4_mx + column_w*cs
+    y = a4_my + row_h*rs
+    w = column_w*(ce-cs+1)
+    h = row_h*(re-rs+1)
+    return x, y, w, h
+
 
 # DRAW IMAGE
-image_span = 2
-image_w = a4_mx + (column_w * image_span)
-draw.rectangle(((a4_mx, 300), (image_w - column_gap, 1500)), fill="#e5e5e5")
-
-
-img_resize(
-    'image.jpg', 'image-resized.jpg', 
-    (image_w - column_gap - a4_mx), 
-    (1500 - 300), 90)
-
 img_featured = Image.open('image-resized.jpg')
-img.paste(img_featured, (a4_mx, 300))
+x, y, w, h = get_coord(0, 1, 1, 12)
+img_resize('image.jpg', 'image-resized.jpg', w, h, 90)
+img.paste(img_featured, (x, y))
 
-# img.show()
-# quit()
+
+
 
 # DRAW TITLE
 title = '''
@@ -111,43 +97,6 @@ font = ImageFont.truetype("arial.ttf", font_size)
 for i, line in enumerate(title_lines):
     draw.text((a4_mx, 1500 + 200 + font_size*1.0*i), line, font=font, fill="black")
 
-
-
-
-
-
-# drap paragraphs
-# y = 2200
-# for i in range(len(paragraphs)):
-#     font_size = 32
-#     font = ImageFont.truetype("arial.ttf", font_size)
-#     paragraph_index = i
-#     if not paragraphs[paragraph_index]:
-#         y += font_size*1.2*2
-#         pass
-#     else:
-#         lines_num = len(paragraphs[paragraph_index])
-
-#         if lines_num == 1:
-#             subtitle = paragraphs[paragraph_index][0]
-#             if subtitle.startswith('## '):
-#                 subtitle = subtitle.replace('## ', '').strip()
-#                 # font_size = 32
-#                 # font = ImageFont.truetype("arial.ttf", font_size)
-#                 draw.text((a4_mx, y), subtitle, font=font, fill="black")
-            
-#         else:
-#             for i, line in enumerate(paragraphs[paragraph_index]):
-#                 if i != lines_num - 1:
-#                     words = line.split(" ")
-#                     words_length = sum(draw.textlength(w, font=font) for w in words)
-#                     space_length = (text_width - words_length) / (len(words) - 1)
-#                     x = 0
-#                     for word in words:
-#                         draw.text((x + a4_mx, y), word, font=font, fill="black")
-#                         x += draw.textlength(word, font=font) + space_length
-#                 else:
-#                     draw.text((a4_mx, y), line, font=font, fill="black")
 
 def draw_text_column(filename, x_start, y_start):
     # DRAW COLUMN
@@ -196,30 +145,48 @@ def draw_text_column(filename, x_start, y_start):
                     else:
                         draw.text((x_start + a4_mx, y), line, font=font, fill="black")
 
+
 draw_text_column('col-1.md', column_w*0, 2200)
 draw_text_column('col-2.md', column_w*1+column_gap//2, 2200)
 draw_text_column('col-3.md', column_w*2+column_gap, 300)
 
-# y = 2200
-# for i in range(len(paragraphs)):
-#     paragraph_index = i
-#     if not paragraphs[paragraph_index]:
-#         y += font_size*1.2*2
-#     else:
-#         lines_num = len(paragraphs[paragraph_index])
-#         for i, line in enumerate(paragraphs[paragraph_index]):
-#             if i != lines_num - 1:
-#                 words = line.split(" ")
-#                 words_length = sum(draw.textlength(w, font=font) for w in words)
-#                 space_length = (text_width - words_length) / (len(words) - 1)
-#                 x = 0
-#                 for word in words:
-#                     draw.text((x + a4_mx, y), word, font=font, fill="black")
-#                     x += draw.textlength(word, font=font) + space_length
-#                 y += font_size*1.2
-#             else:
-#                 draw.text((a4_mx, y), line, font=font, fill="black")
 
 
-# img.show()
+
+
+def debug_margins():
+    draw.line((a4_mx, 0, a4_mx, a4_h), fill='#a21caf')
+    draw.line((a4_w - a4_mx, 0, a4_w - a4_mx, a4_h), fill='#a21caf')
+    draw.line((0, a4_my, a4_w, a4_my), fill='#a21caf')
+    draw.line((0, a4_h - a4_my, a4_w, a4_h - a4_my), fill='#a21caf')
+
+
+def debug_columns():
+    for i in range(column_num-1):
+        # draw.line((column_w*(i+1) + a4_mx - column_gap, 0, column_w*(i+1) + a4_mx  - column_gap, a4_h), fill='#a21caf')
+        draw.line((column_w*(i+1) + a4_mx, 0, column_w*(i+1) + a4_mx, a4_h), fill='#a21caf')
+        # draw.line((column_w*(i+1) + a4_mx + column_gap, 0, column_w*(i+1) + a4_mx  + column_gap, a4_h), fill='#a21caf')
+
+
+def debug_rows():
+    for i in range(row_num-1):
+        draw.line((0, row_h*(i+1) + a4_my, a4_w, row_h*(i+1) + a4_my), fill='#a21caf')
+
+
+def debug_cells():
+    font_size = 64
+    font = ImageFont.truetype("arialbd.ttf", font_size)
+    for i in range(column_num):
+        for k in range(row_num):
+            coord = f'({i}, {k})'
+            draw.text((a4_mx + column_w*i, a4_my + row_h*k), coord, font=font, fill='#a21c')
+
+
+# debug_margins()
+# debug_columns()
+# debug_rows()
+# debug_cells()
+
+
+img.show()
 img.save('template-magazine-1.jpg', quality=50)
