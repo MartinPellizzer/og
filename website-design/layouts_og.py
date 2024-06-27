8849ef5ff3b27dd8ed509600de76ade487121746
Martin <martinpellizzer@gmail.com>
2024-06-27 12:00:52 +0200

    '

--- website-design/layouts_og.py
+++ website-design/layouts_og.py
@@ -5,10 +5,19 @@ import lorem
 # ELEMENTS
 #########################################################################
 
+def spacer_lg():
+    return f'<div class="mb-96"></div>'
+
+
+def spacer_xl():
+    return f'<div class="mb-192"></div>'
+
+
 def title_big(text=None):
     if not text: text = 'Medium length engaging title'
     html = f'<h1 class="text-64 mb-24">{text}</h1>'
     return html
+
 
 def title_mid(text=None, color='text-black'):
     if not text: text = 'Medium length engaging title'
@@ -16,8 +25,8 @@ def title_mid(text=None, color='text-black'):
     return html
 
     
-def paragraph(length=16, color='text-black'):
-    text = (' '.join(lorem.text().split(' ')[:length]) + '.').replace('..', '.')
+def paragraph(text='', length=16, color='text-black'):
+    if text == '': text = (' '.join(lorem.text().split(' ')[:length]) + '.').replace('..', '.')
     return f'<p class="text-18 mb-24 {color}">{text}</p>'
 
 
@@ -41,7 +50,7 @@ def image(src=None, height=600):
 
 
 #########################################################################
-# LAYOUTS
+# HEADERS
 #########################################################################
 
 def header_0000():
@@ -66,6 +75,10 @@ def header_0000():
     return html
 
 
+#########################################################################
+# LAYOUTS
+#########################################################################
+
 def template_0000():
     html = f'''
         <div class="container-xl flex items-center gap-96 mb-48">
@@ -83,22 +96,20 @@ def template_0000():
 
     return html
 
-    
 def template_0001():
     html = f'''
     '''
 
     return html
 
-
-def template_0002():
-    html = f'''
-        <div class="bg-dark">
+def template_content_image(bg_color='', color=''):
+    return f'''
+        <div class="{bg_color}">
             <div class="container-xl flex items-center gap-96">
                 <div class="flex-1">
-                    {title_mid(text='Our Work', color='text-white')}
+                    {title_mid(color=color)}
                     <div class="mb-48"></div>
-                    {paragraph(length=16, color='text-white')}
+                    {paragraph(length=16, color=color)}
                 </div>
                 <div class="flex-1">
                     {image()}
@@ -107,27 +118,65 @@ def template_0002():
         </div>
     '''
 
-    return html
+def template_content_image_ext(theme=''):
+    bg_color = ''
+    color = ''
+    if theme == 'dark':
+        bg_color = 'bg-dark'
+        color = 'text-white'
 
-
-def template_0003():
-    html = f'''
-        <div class="bg-dark flex items-center gap-96">
+    return f'''
+        <div class="{bg_color} flex items-center">
             <div class="flex-1">
-                <div class="container-half ml-auto pl-24">
-                    {title_mid(text='Our Work', color='text-white')}
+                <div class="container-half ml-auto pl-24 mr-96">
+                    {title_mid(color=color)}
                     <div class="mb-48"></div>
-                    {paragraph(length=16, color='text-white')}
+                    {paragraph(length=16, color=color)}
                 </div>
             </div>
             <div class="flex-1">
-                {image()}
+                {image(height=800)}
             </div>
         </div>
     '''
 
-    return html
+def template_image_content(bg_color='', color=''):
+    return f'''
+        <div class="{bg_color}">
+            <div class="container-xl flex items-center gap-96">
+                <div class="flex-1">
+                    {image()}
+                </div>
+                <div class="flex-1">
+                    {title_mid(color=color)}
+                    <div class="mb-48"></div>
+                    {paragraph(length=16, color=color)}
+                </div>
+            </div>
+        </div>
+    '''
 
+def template_image_content_ext(theme=''):
+    bg_color = ''
+    color = ''
+    if theme == 'dark':
+        bg_color = 'bg-dark'
+        color = 'text-white'
+        
+    return f'''
+        <div class="{bg_color} flex items-center">
+            <div class="flex-1">
+                {image(height=800)}
+            </div>
+            <div class="flex-1">
+                <div class="container-half mr-auto pl-24 ml-96">
+                    {title_mid(color=color)}
+                    <div class="mb-48"></div>
+                    {paragraph(length=16, color=color)}
+                </div>
+            </div>
+        </div>
+    '''
 
 def template_0004():
     html = f'''
@@ -161,7 +210,6 @@ def template_0004():
     '''
 
     return html
-
 
 def template_0005():
     image_height = 400
@@ -209,48 +257,211 @@ def template_0005():
     return html
 
 
-def template_0006():
-    html = f'''
-        <div class="">
-            <div class="container-xl flex items-center gap-96">
-                <div class="flex-1">
-                    {title_mid()}
-                    <div class="mb-48"></div>
-                    {paragraph()}
-                </div>
-                <div class="flex-1">
-                    {image()}
-                </div>
+
+
+##################################################################
+# LAYOUTS: IMAGES
+##################################################################
+
+def images_x2():
+    return f'''
+        <div class="container-xl flex items-center">
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
             </div>
         </div>
     '''
     
-    return html
-
-
-def template_0007():
-    html = f'''
-        <div class="">
-            <div class="container-xl flex items-center gap-96">
-                <div class="flex-1">
-                    {image()}
-                </div>
-                <div class="flex-1">
-                    {title_mid()}
-                    <div class="mb-48"></div>
-                    {paragraph()}
-                </div>
+def images_x2_ext():
+    return f'''
+        <div class="flex items-center">
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+        </div>
+    '''
+    
+def images_x3():
+    return f'''
+        <div class="container-xl flex items-center">
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+        </div>
+    '''
+    
+def images_x3_ext():
+    return f'''
+        <div class="flex items-center">
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+        </div>
+    '''
+    
+def images_x4():
+    return f'''
+        <div class="container-xl flex items-center">
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+        </div>
+    '''
+    
+def images_x4_ext():
+    return f'''
+        <div class="flex items-center">
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+        </div>
+    '''
+    
+def images_x5():
+    return f'''
+        <div class="container-xl flex items-center">
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+        </div>
+    '''
+    
+def images_x5_ext():
+    return f'''
+        <div class="flex items-center">
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+        </div>
+    '''
+    
+def images_x6():
+    return f'''
+        <div class="container-xl flex items-center">
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+        </div>
+    '''
+    
+def images_x6_ext():
+    return f'''
+        <div class="flex items-center">
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
+            </div>
+            <div class="flex-1">
+                {image()}
             </div>
         </div>
     '''
 
+
+##################################################################
+# FOOTERS
+##################################################################
+
+def footer_0000():
+    html = f'''
+        <section class="bg-black py-24">
+            <div class="container-xl flex items-center justify-between">
+                <div>
+                    {paragraph('Ozonogroup', color='text-white')}
+                </div>
+                <div class="flex gap-24">
+                    {paragraph('Privacy Policy', color='text-white')}
+                    {paragraph('Terms of Service', color='text-white')}
+                </div>
+            </div>
+        </section>
+    '''
+
     return html
 
-
-templates = [
-    template_0000,
-    template_0001,
-    template_0002,
-    template_0003,
-    template_0004,
-]
